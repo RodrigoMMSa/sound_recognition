965caa527e82e4104df8d98384da2e6087ea5fe9
RodrigoMMSa <rodrigomunizsa@gmail.com>
2019-10-24 13:04:30 +1100

    Kivy interface -- start and Exit

--- App/App.py
+++ App/App.py
@@ -4,24 +4,29 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.pagelayout import PageLayout
 from kivy.uix.button import Button
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('graphics.kv')
 
 
-class Graphics(Widget):
-    def __init__(self, **kwargs):
-        super(Graphics, self).__init__(**kwargs)
+class Graphics(Screen):
+    pass
 
-    def say_hello(self):
-        print("hello")
+
+class NewScreen(Screen):
+    pass
+
+
+screen_manager = ScreenManager()
+screen_manager.add_widget(Graphics(name='graphics'))
+screen_manager.add_widget(NewScreen(name='new_screen'))
 
 
 class MyApp(App):
     def build(self):
-        return Graphics()
+        return screen_manager
 
 
 if __name__ == '__main__':
